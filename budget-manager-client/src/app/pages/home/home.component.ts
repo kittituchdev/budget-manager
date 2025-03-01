9f3e205c585dcc3ef9bbaa3cf7ac374ff94e96cc
kittituch <kittituch.dev@gmail.com>
2025-03-01 18:31:14 +0700

    feat: home layout

--- budget-manager-client/src/app/pages/home/home.component.ts
+++ budget-manager-client/src/app/pages/home/home.component.ts
@@ -1,11 +1,71 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { faBurger } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    FontAwesomeModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  faBurger = faBurger;
+
+  transactions = [
+    {
+      label: 'Food',
+      type: 'expense',
+      category: 'Food/Drink',
+      date: '14 Feb 2025',
+      value: 2450
+    },
+    {
+      label: 'Food',
+      type: 'expense',
+      category: 'Food/Drink',
+      date: '14 Feb 2025',
+      value: 2450
+    },
+    {
+      label: 'Salary',
+      type: 'income',
+      category: 'Salary',
+      date: '14 Feb 2025',
+      value: 200000
+    },
+    {
+      label: 'Food',
+      type: 'expense',
+      category: 'Food/Drink',
+      date: '14 Feb 2025',
+      value: 2450
+    },
+    {
+      label: 'Food',
+      type: 'expense',
+      category: 'Food/Drink',
+      date: '14 Feb 2025',
+      value: 2450
+    },
+    {
+      label: 'Food',
+      type: 'expense',
+      category: 'Food/Drink',
+      date: '14 Feb 2025',
+      value: 2450
+    },
+    {
+      label: 'Food',
+      type: 'expense',
+      category: 'Food/Drink',
+      date: '14 Feb 2025',
+      value: 2450
+    }
+  ]
+
 
 }
